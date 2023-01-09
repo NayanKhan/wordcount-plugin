85e82b164ee9c69cea87632f76268daa01eef954
Nayan Khan <i4u.nayan@gmail.com>
2023-01-09 21:05:59 +0600

    Few Changes

--- word-count.php
+++ word-count.php
@@ -39,8 +39,8 @@ add_filter( 'the_content', 'wordcount_count_words' );
 function wordcount_reading_time( $content ) {
     $stripped_content = strip_tags( $content );
     $wordn            = str_word_count( $stripped_content );
-    $reading_minute   = floor( $wordn / 200 );
-    $reading_seconds  = floor( $wordn % 200 / ( 200 / 60 ) );
+    $reading_minute   = floor( $wordn / 100 );
+    $reading_seconds  = floor( $wordn % 100 / ( 100 / 60 ) );
     $is_visible       = apply_filters( 'wordcount_display_readingtime', 1 );
     if ( $is_visible ) {
         $label   = __( 'Total Reading Time', 'word-count' );
